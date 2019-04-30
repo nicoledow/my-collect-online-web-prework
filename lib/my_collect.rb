05624dc000f9d987572f052eed85d028cdf990b3
Nicole Dow <nikki.dow71@gmail.com>
2019-04-30 00:36:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+list = ["Student 1", "Student 2", "Student 3"]
 
-
+def my_collect(list)
+  collection = []
+  i = 0
+  
+  while i < list.length
+   collection << yield(list[i])
+    i += 1
+  end
+  collection
+end
